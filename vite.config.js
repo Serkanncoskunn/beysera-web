825154d0b8aea77c9fc12d7f09c49fb392a12fe9
serkanncoskunn <serkan_1998.78@hotmail.com>
2026-09-15 00:18:39 +0300

    15.09.2026 Güncelleme - Master Data, TSE Belgeleri ve Optimize Medya Entegrasyonu

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    copyPublicDir: false, // Prevents duplicating ~1GB of master_data media files into dist
-  },
   server: {
     host: true,
     fs: {
